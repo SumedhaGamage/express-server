1194dae6f72cb82093057d3e2f16551d551f9f2f
Sumedha Gamage <sumedhagamage@gmail.com>
2017-05-15 21:57:48 -0500

    package.json modified

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 let app = express();
 
@@ -45,6 +46,6 @@ app.get('/:id/username/:name', (req, res) => {
         year: new Date().getFullYear()
     })
 });
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up in port 3000');
 });
